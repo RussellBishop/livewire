1a78a164543d0200f63708e7a3d65f0505755443
Russell <russell@wearelighthouse.com>
2017-06-13 12:33:13 +0100

    Adding newscard block

---
+++ templates/blocks/newscard.php
@@ -0,0 +1,14 @@
+<div class="border">
+
+    <a href="<?=$post['link'];?>" class="block bg-3 cover ratio-50" style="background-image: url('<?=$post['image'];?>');"></a>
+
+    <div class="pad spacing">
+        <div>
+            <h3><a href="<?=$post['link'];?>"><?=$post['title'];?></a></h3>
+            <p class="text-small o-40">Friday 12th May 2017</p>
+        </div>
+
+        <p><?=$post['description'];?>&hellip;</p>
+    </div>
+
+</div>
