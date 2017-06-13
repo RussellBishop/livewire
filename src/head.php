6ebb8f35d8a7b388bf5e8aebcf8450ba85dda93a
Russell <russell@wearelighthouse.com>
2017-06-13 14:51:15 +0100

    Oops, mobile viewport..

--- src/head.php
+++ src/head.php
@@ -9,9 +9,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title><?=$title?></title>
 
+<?php // mobile meta (hooray!) ?>
 <meta name="HandheldFriendly" content="True">
 <meta name="MobileOptimized" content="320">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
 
 <link href="https://fonts.googleapis.com/css?family=Roboto|Slabo+27px" rel="stylesheet">
 <link href="/dist/css/style.css" rel="stylesheet" />
